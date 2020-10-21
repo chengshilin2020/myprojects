4b04b2671aefc19b3b561798a0858f042f6d86ba
chengshilinw <Qq740704582>
2020-10-21 17:47:45 +0800

    第二次提交

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("hello")
-    public String hello(){
-        return "hello myGit";
+    public String hello(String name){
+        return "hello myGit"+"///////////////"+name;
     }
 }
